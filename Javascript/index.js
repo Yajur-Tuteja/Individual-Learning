166b47d517664726eea4dc47f3d8152babd76543
yajur_tuteja-24 <yajur.tuteja@amdocs.com>
2025-04-11 22:33:49 +0530

    Namaste javascript

---
+++ Javascript/index.js
@@ -0,0 +1,61 @@
+// var x= 9;
+
+// function writeHello () {
+//     console.log("Hello World");
+// }
+
+// writeHello();
+// console.log(writeHello);
+
+// var x;
+// console.log(x);
+
+// if(x === undefined) {
+//     console.log("x is undefined")
+// }
+// else{
+//     console.log("x is not undefined")
+// }
+
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
+
+// console.log(window.a)
+// console.log(a);
+
+// x = 2
+// console.log(x);
+
+// x="posps"
+// console.log(x);
+
+function a() {
+    var y = 9;
+    b();
+    console.log(x);
+    c();
+    function b() {
+        console.log(x);
+    }
+}
+
+function c() {
+    console.log(y);
+}
+
+var x = 10;
+a();
+console.log(y);
+
+
