5b99da3fc95269a7d3a3e3216d891bf583a4c130
yajur_tuteja-24 <yajur.tuteja@amdocs.com>
2025-09-13 14:49:55 +0530

    js changes

--- Javascript/callApplyBind.js
+++ Javascript/callApplyBind.js
@@ -1,4 +1,4 @@
-let name = {
+let name1 = {
     firstName: "Yajur",
     lastName: "Tuteja",
     // printFullName: function () {
@@ -14,9 +14,31 @@ let name2 = {
 // function borrowing
 // name.printFullName.call(name2);
 
-let printFullName = function (home) {
-    console.log(this.firstName + " " + this.lastName + "from" + home);
+let printFullName = function (home, state) {
+    console.log(this.firstName + " " + this.lastName + " from " + home + " , " + state);
 }
-printFullName.call(name, "Delhi");
+printFullName.call(name1, "New Delhi", "Delhi");
+printFullName.apply(name2, ["Mumbai", "Maharashtra"]);
+
+// Bind Method
+
+let printMyName = printFullName.bind(name2,"Mumbai", "Maharashtra")
+
+// console.log(printMyName);
+// printMyName();
+// printMyName();
+
+// polyfill for bind
+
+Function.prototype.mybind = function(target,...args) {
+    obj = this; 
+    console.log(args);
+    return function() {
+        obj.call(target,...args);
+    }
+} 
+
+let printMyName2 = printFullName.mybind(name1,"Chandigarh","Punjab")
+printMyName2()
 
 
