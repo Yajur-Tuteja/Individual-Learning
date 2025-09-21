3acb9aa0a823e8111a3fd0a86d77b3faf3159455
yajur_tuteja-24 <yajur.tuteja@amdocs.com>
2025-09-21 17:01:37 +0530

    Chapter 9 and 10

--- React/src/components/Body.js
+++ React/src/components/Body.js
@@ -122,18 +122,18 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="flex">
+        <div className="m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
           <button
-            className="search-btn"
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               // filter the restaurant cards and update the UI
               // need searchtext
@@ -145,18 +145,21 @@ const Body = () => {
             }}
           >Search</button>
         </div>
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = resList.filter(
-              (res) => res.info.avgRating > 4.3
-            );
-            setFilteredResList(filteredList);
-            console.log(filteredList);
-          }}
-        >Top Rated Restaurants</button>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = resList.filter(
+                (res) => res.info.avgRating > 4.3
+              );
+              setFilteredResList(filteredList);
+              console.log(filteredList);
+            }}
+          >Top Rated Restaurants</button>
+        </div>
+        
       </div>
-      <div className="res-container">
+      <div className="flex flex-wrap">
         {
           filteredResList.map((res) => (
             <Link
